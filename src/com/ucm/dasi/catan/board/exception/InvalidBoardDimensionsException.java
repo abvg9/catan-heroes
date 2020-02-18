dce185e87a19c29bb542a60c0c6a4064c5c86369
Roberto Pintos López <roberto@secture.com>
2020-02-18 17:30:29 +0100

    Added logic.

---
+++ src/com/ucm/dasi/catan/board/exception/InvalidBoardDimensionsException.java
@@ -0,0 +1,10 @@
+package com.ucm.dasi.catan.board.exception;
+
+public class InvalidBoardDimensionsException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7651089991744581056L;
+
+}
