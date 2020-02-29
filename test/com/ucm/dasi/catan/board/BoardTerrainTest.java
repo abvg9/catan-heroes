b715ea223ddd12bf5b2b48623d50b7c1daf27e50
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-02-29 13:49:30 +0100

    Added terrain tests.

---
+++ test/com/ucm/dasi/catan/board/BoardTerrainTest.java
@@ -0,0 +1,32 @@
+package com.ucm.dasi.catan.board;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class BoardTerrainTest {
+
+	@Test
+	public void itMustReturnTheRightElementType() {
+		BoardTerrain terrain = new BoardTerrain(1, TerrainType.None);
+		
+		assertEquals(BoardElementType.Terrain, terrain.getElementType());
+	}
+	
+	@Test
+	public void itMustGetItsProductionNumber() {
+		int productionNumber = 1;
+		BoardTerrain terrain = new BoardTerrain(productionNumber, TerrainType.None);
+		
+		assertEquals(productionNumber, terrain.getProductionNumber());
+	}
+	
+	@Test
+	public void itMustGetItsStructureType() {
+		TerrainType type = TerrainType.None;
+		BoardTerrain terrain = new BoardTerrain(1, type);
+		
+		assertEquals(type, terrain.getType());
+	}
+	
+}
