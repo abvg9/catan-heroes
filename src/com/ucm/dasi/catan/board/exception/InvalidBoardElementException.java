8ff3f084c4cbba48f3aa7964b4b5c0ecd51ea52d
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-03-03 18:58:40 +0100

    Exception refactor.

--- src/com/ucm/dasi/catan/board/exception/InvalidBoardElementException.java
+++ src/com/ucm/dasi/catan/board/exception/InvalidBoardElementException.java
@@ -2,16 +2,9 @@ package com.ucm.dasi.catan.board.exception;
 
 public class InvalidBoardElementException extends Exception {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -514121800286703032L;
-
-	public InvalidBoardElementException() {
-		super();
-	}
 	
-	public InvalidBoardElementException(String message) {
-		super(message);
+	public InvalidBoardElementException() {
+		super("Invalid board element");
 	}
 }
