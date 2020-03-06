2d110287a60cd03cef02dd5e13a8f4bb87c47198
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-03-06 19:43:07 +0100

    Added player tests.

---
+++ test/com/ucm/dasi/catan/player/PlayerTest.java
@@ -0,0 +1,28 @@
+package com.ucm.dasi.catan.player;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.ucm.dasi.catan.warehouse.IWarehouse;
+import com.ucm.dasi.catan.warehouse.Warehouse;
+import com.ucm.dasi.catan.warehouse.exception.NegativeNumberException;
+
+public class PlayerTest {
+
+    @Test
+    public void itMustReturnItsId() throws NegativeNumberException {
+	int id = 0;
+	Player player = new Player(id, new Warehouse());
+	
+	assertEquals(id, player.getId());
+    }
+    
+    @Test
+    public void itMustReturnItsWarehouse() throws NegativeNumberException {
+	IWarehouse warehouse = new Warehouse();
+	Player player = new Player(0, warehouse);
+	
+	assertEquals(warehouse, player.getWarehouse());
+    }
+}
