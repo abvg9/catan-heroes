0ca52d21c8255c946f53f56af6fcc49c7ee8c000
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-03-12 11:54:34 +0100

    Added request types

---
+++ src/com/ucm/dasi/catan/request/BuildElementRequest.java
@@ -0,0 +1,33 @@
+package com.ucm.dasi.catan.request;
+
+import com.ucm.dasi.catan.board.BoardElementType;
+import com.ucm.dasi.catan.player.IPlayer;
+
+public class BuildElementRequest extends BaseRequest {
+
+    protected BoardElementType type;
+
+    protected int x;
+
+    protected int y;
+
+    public BuildElementRequest(IPlayer player, BoardElementType type, int x, int y) {
+	super(player);
+
+	this.type = type;
+	this.x = x;
+	this.y = y;
+    }
+
+    public BoardElementType getType() {
+	return this.type;
+    }
+
+    public int getX() {
+	return this.x;
+    }
+
+    public int getY() {
+	return this.y;
+    }
+}
---
+++ src/com/ucm/dasi/catan/request/IRequest.java
@@ -0,0 +1,7 @@
+package com.ucm.dasi.catan.request;
+
+import com.ucm.dasi.catan.player.IPlayer;
+
+public interface IRequest {
+    IPlayer getPlayer();
+}
