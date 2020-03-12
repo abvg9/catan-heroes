5bb4736821b33282cdbc9ea63afa07129f528a93
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-03-12 18:31:48 +0100

    Added missing tests

--- test/com/ucm/dasi/catan/board/element/MinimunOwnedElement.java
+++ test/com/ucm/dasi/catan/board/element/MinimunOwnedElement.java
@@ -2,11 +2,11 @@ package com.ucm.dasi.catan.board.element;
 
 import com.ucm.dasi.catan.board.BoardElementType;
 import com.ucm.dasi.catan.player.IPlayer;
-import com.ucm.dasi.catan.warehouse.Warehouse;
+import com.ucm.dasi.catan.warehouse.IWarehouse;
 
 public class MinimunOwnedElement extends OwnedElement {
 
-    public MinimunOwnedElement(BoardElementType type, IPlayer owner) {
-	super(type, new Warehouse(), owner);
+    public MinimunOwnedElement(BoardElementType type, IWarehouse cost,  IPlayer owner) {
+	super(type, cost, owner);
     }
 }
