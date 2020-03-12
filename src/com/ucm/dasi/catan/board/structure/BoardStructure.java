868ebff33464a46075dbe57e1848353be03e10f4
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-03-12 19:02:53 +0100

    Code styles has been applied

--- src/com/ucm/dasi/catan/board/structure/BoardStructure.java
+++ src/com/ucm/dasi/catan/board/structure/BoardStructure.java
@@ -12,12 +12,12 @@ import com.ucm.dasi.catan.warehouse.Warehouse;
 import com.ucm.dasi.catan.warehouse.exception.NegativeNumberException;
 
 public class BoardStructure extends OwnedElement implements IBoardStructure {
-    
+
     private StructureType type;
 
     public BoardStructure(IPlayer owner, StructureType type) {
 	super(BoardElementType.Structure, buildCostFromType(type), owner);
-	
+
 	this.type = type;
     }
 
@@ -25,19 +25,19 @@ public class BoardStructure extends OwnedElement implements IBoardStructure {
     public StructureType getType() {
 	return type;
     }
-    
+
     private static IWarehouse buildCityCost() {
 	Map<ResourceType, Integer> costMap = new TreeMap<ResourceType, Integer>();
 	costMap.put(ResourceType.Grain, 2);
 	costMap.put(ResourceType.Ore, 3);
-	
+
 	try {
 	    return new Warehouse(costMap);
 	} catch (NegativeNumberException e) {
 	    return new Warehouse();
 	}
     }
-    
+
     private static IWarehouse buildCostFromType(StructureType type) {
 	switch (type) {
 	case City:
@@ -48,14 +48,14 @@ public class BoardStructure extends OwnedElement implements IBoardStructure {
 	    return new Warehouse();
 	}
     }
-    
+
     private static IWarehouse buildSettlementCost() {
 	Map<ResourceType, Integer> costMap = new TreeMap<ResourceType, Integer>();
 	costMap.put(ResourceType.Brick, 1);
 	costMap.put(ResourceType.Grain, 1);
 	costMap.put(ResourceType.Lumber, 1);
 	costMap.put(ResourceType.Wool, 1);
-	
+
 	try {
 	    return new Warehouse(costMap);
 	} catch (NegativeNumberException e) {
