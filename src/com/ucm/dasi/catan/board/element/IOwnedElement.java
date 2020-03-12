6359e61f87e08c48e3087bf6ce647a81d2baa9ba
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-03-12 18:12:17 +0100

    Added owned element costs

--- src/com/ucm/dasi/catan/board/element/IOwnedElement.java
+++ src/com/ucm/dasi/catan/board/element/IOwnedElement.java
@@ -1,8 +1,16 @@
 package com.ucm.dasi.catan.board.element;
 
 import com.ucm.dasi.catan.player.IPlayer;
+import com.ucm.dasi.catan.warehouse.IWarehouse;
 
 public interface IOwnedElement extends IBoardElement {
+    
+    /**
+     * Returns the element's cost.
+     * @return Element's cost.
+     */
+    IWarehouse getCost();
+    
     /**
      * Gets the elemnt's owner
      * @return Player that owns the element or null if no player owns the element.
--- src/com/ucm/dasi/catan/board/element/OwnedElement.java
+++ src/com/ucm/dasi/catan/board/element/OwnedElement.java
@@ -2,18 +2,34 @@ package com.ucm.dasi.catan.board.element;
 
 import com.ucm.dasi.catan.board.BoardElementType;
 import com.ucm.dasi.catan.player.IPlayer;
+import com.ucm.dasi.catan.warehouse.IWarehouse;
+import com.ucm.dasi.catan.warehouse.Warehouse;
+import com.ucm.dasi.catan.warehouse.exception.NegativeNumberException;
 
 public abstract class OwnedElement extends BoardElement implements IOwnedElement {
 
+    private IWarehouse cost;
+    
     private IPlayer owner;
     
-    public OwnedElement(BoardElementType type, IPlayer owner) {
+    public OwnedElement(BoardElementType type, IWarehouse cost, IPlayer owner) {
 	super(type);
 	
+	this.cost = cost;
 	this.owner = owner;
     }
     
+    @Override
+    public IWarehouse getCost() {
+	try {
+	    return new Warehouse(cost);
+	} catch (NegativeNumberException e) {
+	    return null;
+	}
+    }
+    
+    @Override
     public IPlayer getOwner() {
-	return this.owner;
+	return owner;
     }
 }
