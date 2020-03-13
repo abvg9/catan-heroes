9071d85633ca9a1cfc1d6e99b0200b37a17f8168
Bob <roberto.pintos.lopez@gmail.com>
2020-03-13 13:15:42 +0100

    Catan board test build elements throught common methods

--- test/com/ucm/dasi/catan/board/CatanBoardTest.java
+++ test/com/ucm/dasi/catan/board/CatanBoardTest.java
@@ -7,25 +7,38 @@ import org.junit.Test;
 
 import com.ucm.dasi.catan.board.connection.BoardConnection;
 import com.ucm.dasi.catan.board.connection.ConnectionType;
+import com.ucm.dasi.catan.board.connection.IBoardConnection;
 import com.ucm.dasi.catan.board.element.IBoardElement;
 import com.ucm.dasi.catan.board.exception.InvalidBoardDimensionsException;
 import com.ucm.dasi.catan.board.exception.InvalidBoardElementException;
 import com.ucm.dasi.catan.board.structure.BoardStructure;
+import com.ucm.dasi.catan.board.structure.IBoardStructure;
 import com.ucm.dasi.catan.board.structure.StructureType;
 import com.ucm.dasi.catan.board.terrain.BoardTerrain;
+import com.ucm.dasi.catan.board.terrain.IBoardTerrain;
 import com.ucm.dasi.catan.board.terrain.TerrainType;
 
 public class CatanBoardTest {
 
+    private IBoardTerrain createNoneTerrain() {
+	return new BoardTerrain(0, TerrainType.None);
+    }
+
+    private IBoardStructure createNoneStructure() {
+	return new BoardStructure(null, StructureType.None);
+    }
+
+    private IBoardConnection createVoidConnection() {
+	return new BoardConnection(null, ConnectionType.Void);
+    }
+
     @Test
     public void itMustBeInitializable() throws InvalidBoardDimensionsException, InvalidBoardElementException {
-	IBoardElement[][] elements = {
-		{ new BoardStructure(null, StructureType.None), new BoardConnection(null, ConnectionType.Void),
-			new BoardStructure(null, StructureType.None), },
-		{ new BoardConnection(null, ConnectionType.Void), new BoardTerrain(0, TerrainType.None),
-			new BoardConnection(null, ConnectionType.Void), },
-		{ new BoardStructure(null, StructureType.None), new BoardConnection(null, ConnectionType.Void),
-			new BoardStructure(null, StructureType.None), }, };
+
+	IBoardElement[][] elements = { { createNoneStructure(), createVoidConnection(), createNoneStructure(), },
+		{ createVoidConnection(), createNoneTerrain(), createVoidConnection(), },
+		{ createNoneStructure(), createVoidConnection(), new BoardStructure(null, StructureType.None), }, };
+
 	CatanBoard board = new CatanBoard(3, 3, elements);
 
 	assertNotEquals(null, board);
@@ -33,14 +46,12 @@ public class CatanBoardTest {
 
     @Test
     public void itMustGetAnElementByItsPosition() throws InvalidBoardDimensionsException, InvalidBoardElementException {
-	IBoardElement element = new BoardStructure(null, StructureType.None);
-	IBoardElement[][] elements = {
-		{ element, new BoardConnection(null, ConnectionType.Void),
-			new BoardStructure(null, StructureType.None), },
-		{ new BoardConnection(null, ConnectionType.Void), new BoardTerrain(0, TerrainType.None),
-			new BoardConnection(null, ConnectionType.Void), },
-		{ new BoardStructure(null, StructureType.None), new BoardConnection(null, ConnectionType.Void),
-			new BoardStructure(null, StructureType.None), }, };
+
+	IBoardElement element = createNoneStructure();
+	IBoardElement[][] elements = { { element, createVoidConnection(), createNoneStructure(), },
+		{ createVoidConnection(), createNoneTerrain(), createVoidConnection(), },
+		{ createNoneStructure(), createVoidConnection(), createNoneStructure(), }, };
+
 	CatanBoard board = new CatanBoard(3, 3, elements);
 
 	assertEquals(element, board.get(0, 0));
@@ -48,14 +59,12 @@ public class CatanBoardTest {
 
     @Test
     public void itMustGetATerrainByItsPosition() throws InvalidBoardDimensionsException, InvalidBoardElementException {
-	IBoardElement element = new BoardTerrain(0, TerrainType.None);
-	IBoardElement[][] elements = {
-		{ new BoardStructure(null, StructureType.None), new BoardConnection(null, ConnectionType.Void),
-			new BoardStructure(null, StructureType.None), },
-		{ new BoardConnection(null, ConnectionType.Void), element,
-			new BoardConnection(null, ConnectionType.Void), },
-		{ new BoardStructure(null, StructureType.None), new BoardConnection(null, ConnectionType.Void),
-			new BoardStructure(null, StructureType.None), }, };
+
+	IBoardElement element = createNoneTerrain();
+	IBoardElement[][] elements = { { createNoneStructure(), createVoidConnection(), createNoneStructure(), },
+		{ createVoidConnection(), element, createVoidConnection(), },
+		{ createNoneStructure(), createVoidConnection(), createNoneStructure(), }, };
+
 	CatanBoard board = new CatanBoard(3, 3, elements);
 
 	assertEquals(element, board.getTerrain(0, 0));
@@ -64,14 +73,12 @@ public class CatanBoardTest {
     @Test
     public void itMustGetAnStructureByItsPosition()
 	    throws InvalidBoardDimensionsException, InvalidBoardElementException {
-	IBoardElement element = new BoardStructure(null, StructureType.None);
-	IBoardElement[][] elements = {
-		{ element, new BoardConnection(null, ConnectionType.Void),
-			new BoardStructure(null, StructureType.None), },
-		{ new BoardConnection(null, ConnectionType.Void), new BoardTerrain(0, TerrainType.None),
-			new BoardConnection(null, ConnectionType.Void), },
-		{ new BoardStructure(null, StructureType.None), new BoardConnection(null, ConnectionType.Void),
-			new BoardStructure(null, StructureType.None), }, };
+
+	IBoardElement element = createNoneStructure();
+	IBoardElement[][] elements = { { element, createVoidConnection(), createNoneStructure(), },
+		{ createVoidConnection(), createNoneTerrain(), createVoidConnection(), },
+		{ createNoneStructure(), createVoidConnection(), createNoneStructure(), }, };
+
 	CatanBoard board = new CatanBoard(3, 3, elements);
 
 	assertEquals(element, board.getStructure(0, 0));
@@ -80,39 +87,33 @@ public class CatanBoardTest {
     @Test(expected = InvalidBoardElementException.class)
     public void itMustFailIfAFalseConnectionIsProvided()
 	    throws InvalidBoardDimensionsException, InvalidBoardElementException {
-	IBoardElement[][] elements = {
-		{ new BoardStructure(null, StructureType.None), new BoardStructure(null, StructureType.None),
-			new BoardStructure(null, StructureType.None), },
-		{ new BoardConnection(null, ConnectionType.Void), new BoardTerrain(0, TerrainType.None),
-			new BoardConnection(null, ConnectionType.Void), },
-		{ new BoardStructure(null, StructureType.None), new BoardConnection(null, ConnectionType.Void),
-			new BoardStructure(null, StructureType.None), }, };
+
+	IBoardElement[][] elements = { { createNoneStructure(), createNoneStructure(), createNoneStructure(), },
+		{ createVoidConnection(), createNoneTerrain(), createVoidConnection(), },
+		{ createNoneStructure(), createVoidConnection(), createNoneStructure(), }, };
+
 	new CatanBoard(3, 3, elements);
     }
 
     @Test(expected = InvalidBoardElementException.class)
     public void itMustFailIfAFalseTerrainIsProvided()
 	    throws InvalidBoardDimensionsException, InvalidBoardElementException {
-	IBoardElement[][] elements = {
-		{ new BoardStructure(null, StructureType.None), new BoardConnection(null, ConnectionType.Void),
-			new BoardStructure(null, StructureType.None), },
-		{ new BoardConnection(null, ConnectionType.Void), new BoardConnection(null, ConnectionType.Void),
-			new BoardConnection(null, ConnectionType.Void), },
-		{ new BoardStructure(null, StructureType.None), new BoardConnection(null, ConnectionType.Void),
-			new BoardStructure(null, StructureType.None), }, };
+
+	IBoardElement[][] elements = { { createNoneStructure(), createVoidConnection(), createNoneStructure(), },
+		{ createVoidConnection(), createVoidConnection(), createVoidConnection(), },
+		{ createNoneStructure(), createVoidConnection(), createNoneStructure(), }, };
+
 	new CatanBoard(3, 3, elements);
     }
 
     @Test(expected = InvalidBoardElementException.class)
     public void itMustFailIfAFalseStructureIsProvided()
 	    throws InvalidBoardDimensionsException, InvalidBoardElementException {
-	IBoardElement[][] elements = {
-		{ new BoardTerrain(0, TerrainType.None), new BoardConnection(null, ConnectionType.Void),
-			new BoardStructure(null, StructureType.None), },
-		{ new BoardConnection(null, ConnectionType.Void), new BoardTerrain(0, TerrainType.None),
-			new BoardConnection(null, ConnectionType.Void), },
-		{ new BoardStructure(null, StructureType.None), new BoardConnection(null, ConnectionType.Void),
-			new BoardStructure(null, StructureType.None), }, };
+	
+	IBoardElement[][] elements = { { createNoneTerrain(), createVoidConnection(), createNoneStructure(), },
+		{ createVoidConnection(), createNoneTerrain(), createVoidConnection(), },
+		{ createNoneStructure(), createVoidConnection(), createNoneStructure(), }, };
+	
 	new CatanBoard(3, 3, elements);
     }
 }
