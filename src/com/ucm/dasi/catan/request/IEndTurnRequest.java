8985e2a79fc8f6c6ce193a15d46e464557d44c62
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-03-21 15:03:17 +0100

    Added turn requests

---
+++ src/com/ucm/dasi/catan/request/IEndTurnRequest.java
@@ -0,0 +1,4 @@
+package com.ucm.dasi.catan.request;
+
+public interface IEndTurnRequest extends IRequest {
+}
--- src/com/ucm/dasi/catan/request/RequestType.java
+++ src/com/ucm/dasi/catan/request/RequestType.java
@@ -1,5 +1,5 @@
 package com.ucm.dasi.catan.request;
 
 public enum RequestType {
-    BuildConnection, BuildStructure
+    BuildConnection, BuildStructure, EndTurn, StartTurn
 }
