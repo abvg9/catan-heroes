3aeed6791acc67f43d4f4315639f642d1905713f
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-03-21 13:14:44 +0100

    Added turn constructor parameter

--- test/com/ucm/dasi/catan/game/CatanGameEngineTest.java
+++ test/com/ucm/dasi/catan/game/CatanGameEngineTest.java
@@ -56,7 +56,7 @@ public class CatanGameEngineTest {
 	    fail();
 	};
 
-	CatanGameEngine engine = new CatanGameEngine(board, players, errorHandler);
+	CatanGameEngine engine = new CatanGameEngine(board, players, 0, errorHandler);
 
 	int requestX = 2;
 	int requestY = 2;
@@ -95,7 +95,7 @@ public class CatanGameEngineTest {
 	    fail();
 	};
 
-	CatanGameEngine engine = new CatanGameEngine(board, players, errorHandler);
+	CatanGameEngine engine = new CatanGameEngine(board, players, 0, errorHandler);
 
 	int requestX = 3;
 	int requestY = 2;
