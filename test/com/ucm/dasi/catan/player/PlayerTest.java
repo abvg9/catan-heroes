6297696a1f9c0e5e27cb62e68d859983d1770eaa
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-03-22 16:32:34 +0100

    Added gradle build.
    Added JUnit library as implementation

--- test/com/ucm/dasi/catan/player/PlayerTest.java
+++ test/com/ucm/dasi/catan/player/PlayerTest.java
@@ -1,8 +1,8 @@
 package com.ucm.dasi.catan.player;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import com.ucm.dasi.catan.warehouse.IWarehouse;
 import com.ucm.dasi.catan.warehouse.Warehouse;
