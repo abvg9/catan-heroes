454eacb19adb386f9124f24fc67fd5d64371cd03
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-03-24 14:14:35 +0100

    Added board test

--- test/com/ucm/dasi/catan/board/CatanBoardTest.java
+++ test/com/ucm/dasi/catan/board/CatanBoardTest.java
@@ -10,14 +10,22 @@ import com.ucm.dasi.catan.board.connection.IBoardConnection;
 import com.ucm.dasi.catan.board.element.IBoardElement;
 import com.ucm.dasi.catan.board.exception.InvalidBoardDimensionsException;
 import com.ucm.dasi.catan.board.exception.InvalidBoardElementException;
+import com.ucm.dasi.catan.board.group.StructureTerrainTypesPair;
 import com.ucm.dasi.catan.board.structure.BoardStructure;
 import com.ucm.dasi.catan.board.structure.IBoardStructure;
 import com.ucm.dasi.catan.board.structure.StructureType;
 import com.ucm.dasi.catan.board.terrain.BoardTerrain;
 import com.ucm.dasi.catan.board.terrain.IBoardTerrain;
 import com.ucm.dasi.catan.board.terrain.TerrainType;
+import com.ucm.dasi.catan.player.IPlayer;
+import com.ucm.dasi.catan.player.Player;
 import com.ucm.dasi.catan.resource.ResourceManager;
+import com.ucm.dasi.catan.resource.exception.NegativeNumberException;
+import com.ucm.dasi.catan.resource.production.IResourceProduction;
+import com.ucm.dasi.catan.resource.provider.ITerrainProductionProvider;
 import com.ucm.dasi.catan.resource.provider.TerrainProductionProvider;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 public class CatanBoardTest {
@@ -90,6 +98,43 @@ public class CatanBoardTest {
     assertEquals(element, board.getStructure(0, 0));
   }
 
+  @DisplayName("It must get a player's production")
+  @Tag("CatanBoard")
+  @Test
+  public void itMustGetAPlayerProduction()
+      throws InvalidBoardDimensionsException, InvalidBoardElementException,
+          NegativeNumberException {
+
+    int targetProductionNumber = 3;
+    IPlayer player = new Player(1, new ResourceManager());
+
+    IBoardElement[][] elements = {
+      {
+        createNoneStructure(), createVoidConnection(), createSettlementStructure(player),
+      },
+      {
+        createVoidConnection(),
+        createMountainsTerrain(targetProductionNumber),
+        createVoidConnection(),
+      },
+      {
+        createNoneStructure(), createVoidConnection(), createNoneStructure(),
+      },
+    };
+
+    ITerrainProductionProvider terrainProductionProvider =
+        TerrainProductionProvider.buildDefaultProvider();
+
+    CatanBoard board = new CatanBoard(3, 3, elements, terrainProductionProvider);
+
+    IResourceProduction production = board.getProduction(targetProductionNumber);
+
+    assertEquals(
+        terrainProductionProvider.getResourceManager(
+            new StructureTerrainTypesPair(StructureType.Settlement, TerrainType.Mountains)),
+        production.getProduction(player));
+  }
+
   @Test()
   public void itMustFailIfAFalseConnectionIsProvided()
       throws InvalidBoardDimensionsException, InvalidBoardElementException {
@@ -157,8 +202,16 @@ public class CatanBoardTest {
     return new BoardTerrain(0, TerrainType.None);
   }
 
+  private IBoardTerrain createMountainsTerrain(int productionNumber) {
+    return new BoardTerrain(productionNumber, TerrainType.Mountains);
+  }
+
   private IBoardStructure createNoneStructure() {
     return new BoardStructure(null, new ResourceManager(), StructureType.None);
+  }
+
+  private IBoardStructure createSettlementStructure(IPlayer owner) {
+    return new BoardStructure(owner, new ResourceManager(), StructureType.Settlement);
   }
 
   private IBoardConnection createVoidConnection() {
