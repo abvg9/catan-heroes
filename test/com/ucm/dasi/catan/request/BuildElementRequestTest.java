b416dc7150315af3a4127c1d516a0e6f3d4f5e46
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-03-25 12:36:21 +0100

    NegativeNumberException is now a runtime exception.
    Code has been simplified.

--- test/com/ucm/dasi/catan/request/BuildElementRequestTest.java
+++ test/com/ucm/dasi/catan/request/BuildElementRequestTest.java
@@ -6,7 +6,6 @@ import com.ucm.dasi.catan.board.BoardElementType;
 import com.ucm.dasi.catan.player.IPlayer;
 import com.ucm.dasi.catan.player.Player;
 import com.ucm.dasi.catan.resource.ResourceManager;
-import com.ucm.dasi.catan.resource.exception.NegativeNumberException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -16,7 +15,7 @@ public class BuildElementRequestTest {
   @DisplayName("It must store its initial values")
   @Tag(value = "BuildElementRequest")
   @Test
-  public void mustStoreInitialValues() throws NegativeNumberException {
+  public void mustStoreInitialValues() {
     IPlayer player = new Player(0, new ResourceManager());
     BoardElementType type = BoardElementType.Connection;
     int x = 0;
