b929fc0c8c54f20ac391e7992369502145af6d6f
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-03-25 09:37:38 +0100

    Added resource storage.
    Added a resource storage class and interface.
    Resource manager now extends the resource storage class.
    Added tests.
    Moved resource manager test to resource storage tests.

--- src/com/ucm/dasi/catan/resource/IResourceManager.java
+++ src/com/ucm/dasi/catan/resource/IResourceManager.java
@@ -3,15 +3,9 @@ package com.ucm.dasi.catan.resource;
 import com.ucm.dasi.catan.resource.exception.NegativeNumberException;
 import com.ucm.dasi.catan.resource.exception.NotEnoughtResourcesException;
 
-public interface IResourceManager {
+public interface IResourceManager extends IResourceStorage {
 
-  /* Methods */
+  void add(IResourceStorage resourceManager) throws NegativeNumberException;
 
-  void add(IResourceManager resourcesToPay) throws NegativeNumberException;
-
-  int getQuantityResource();
-
-  int getResource(ResourceType resource);
-
-  void substract(IResourceManager resourcesToPay) throws NotEnoughtResourcesException;
+  void substract(IResourceStorage resourceManager) throws NotEnoughtResourcesException;
 }
---
+++ src/com/ucm/dasi/catan/resource/ResourceStorage.java
@@ -0,0 +1,77 @@
+package com.ucm.dasi.catan.resource;
+
+import com.ucm.dasi.catan.resource.exception.NegativeNumberException;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class ResourceStorage {
+
+  protected int resourceQuantity;
+
+  private TreeMap<ResourceType, Integer> resources;
+
+  public ResourceStorage() {
+
+    resources = new TreeMap<ResourceType, Integer>();
+    resourceQuantity = 0;
+
+    for (ResourceType resourceType : ResourceType.values()) {
+      resources.put(resourceType, 0);
+    }
+  }
+
+  public ResourceStorage(Map<ResourceType, Integer> resources) throws NegativeNumberException {
+    this();
+
+    for (ResourceType resourceType : ResourceType.values()) {
+      Integer resourceAmount = resources.get(resourceType);
+      if (null == resourceAmount) {
+        resourceAmount = 0;
+      }
+      setResource(resourceType, resourceAmount);
+    }
+  }
+
+  public ResourceStorage(IResourceStorage resourceManager) throws NegativeNumberException {
+    this();
+
+    for (ResourceType resourceType : ResourceType.values()) {
+      setResource(resourceType, resourceManager.getResource(resourceType));
+    }
+  }
+
+  @Override
+  public boolean equals(Object object) {
+    if (!(object instanceof IResourceManager)) {
+      return false;
+    }
+
+    for (ResourceType resourceType : ResourceType.values()) {
+      if (this.getResource(resourceType) != ((IResourceManager) object).getResource(resourceType)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  public int getResource(ResourceType type) {
+    return resources.get(type);
+  }
+
+  public int getQuantityResource() {
+    return resourceQuantity;
+  }
+
+  protected void setResource(ResourceType type, int newQuantity) throws NegativeNumberException {
+
+    if (newQuantity < 0) {
+      throw new NegativeNumberException(type.toString());
+    }
+
+    int oldQuantity = resources.get(type);
+    resourceQuantity += newQuantity - oldQuantity;
+
+    resources.put(type, newQuantity);
+  }
+}
