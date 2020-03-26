b194e8cef31d90e96292195ab9cf1e796bd04a87
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-03-26 20:18:46 +0100

    Added game state

---
+++ src/com/ucm/dasi/catan/game/GameState.java
@@ -0,0 +1,21 @@
+package com.ucm.dasi.catan.game;
+
+/**
+ * Represents a state of a Catan game
+ */
+public enum GameState {
+  /**
+   * The game is ended. A set of winners are declared.
+   */
+  ENDED,
+  /**
+   * The game is in the foundation phase.
+   * 
+   * This phase is the initial one. Players take turns to place their initial elements for free.
+   */
+  FOUNDATION,
+  /**
+   * The game is in the normal phase.
+   */
+  NORMAL
+}
