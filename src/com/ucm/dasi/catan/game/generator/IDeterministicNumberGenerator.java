3ca44bc6b75a269676874668400d7d3131e24d0b
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-03-26 18:12:18 +0100

    Added number generators

---
+++ src/com/ucm/dasi/catan/game/generator/IDeterministicNumberGenerator.java
@@ -0,0 +1,18 @@
+package com.ucm.dasi.catan.game.generator;
+
+/**
+ * Represents a deterministic production number generator.
+ */
+public interface IDeterministicNumberGenerator extends INumberGenerator {
+  /**
+   * Gets a production number with an offset relative to the next production number.
+   * 
+   * An offset of zero is equivalent to request the next production number.
+   * A positive offset is equivalent to request production numbers next to the next one.
+   * A negative offset is equivalent to request production numbers previous to the next one.
+   * 
+   * @param offset Request offset.
+   * @return Production number obtained.
+   */
+  public int getProductionNumber(int offset);
+}
