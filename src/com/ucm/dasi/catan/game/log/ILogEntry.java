235630f34fc2cb8bb7794bd6254c0f5b58ec5745
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-03-30 18:36:52 +0200

    Added game log

---
+++ src/com/ucm/dasi/catan/game/log/ILogEntry.java
@@ -0,0 +1,10 @@
+package com.ucm.dasi.catan.game.log;
+
+import com.ucm.dasi.catan.request.IRequest;
+
+public interface ILogEntry {
+ 
+  public int getProductionNumber();
+  
+  public Iterable<IRequest> getRequests();
+}
