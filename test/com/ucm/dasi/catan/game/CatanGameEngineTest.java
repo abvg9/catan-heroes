caf29c8f263b49b6a3b946f9002693cac0eee463
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-03-30 09:55:41 +0200

    Added test

--- test/com/ucm/dasi/catan/game/CatanGameEngineTest.java
+++ test/com/ucm/dasi/catan/game/CatanGameEngineTest.java
@@ -47,6 +47,48 @@ import org.junit.jupiter.api.Test;
 
 public class CatanGameEngineTest {
 
+  @DisplayName("it must end the game if the active player won")
+  @Tag("CatanBoardEngine")
+  @Test
+  public void itMustEndTheGameIfTheActivePlayerWon()
+      throws InvalidBoardDimensionsException, InvalidBoardElementException, NonNullInputException,
+          NonVoidCollectionException {
+
+    Map<ResourceType, Integer> playerResources = new TreeMap<ResourceType, Integer>();
+
+    playerResources.put(ResourceType.BRICK, 1);
+    playerResources.put(ResourceType.GRAIN, 1);
+    playerResources.put(ResourceType.LUMBER, 1);
+    playerResources.put(ResourceType.WOOL, 1);
+
+    IPlayer player1 = new Player(0, new ResourceManager(playerResources));
+    IPlayer[] players = {player1};
+
+    ICatanEditableBoard board = buildStandardBoard(player1);
+
+    Consumer<IRequest> errorHandler =
+        (request) -> {
+          fail();
+        };
+
+    CatanGameEngine engine =
+        new CatanGameEngine(
+            board, players, 1, GameState.NORMAL, 0, true, errorHandler, new CatanRandomGenerator());
+
+    int requestX = 2;
+    int requestY = 2;
+    
+    IRequest[] requests = {
+        new BuildStructureRequest(player1, StructureType.SETTLEMENT, requestX, requestY),
+      new EndTurnRequest(player1)
+    };
+
+    engine.processRequests(requests);
+
+    assertSame(GameState.ENDED, engine.getState());
+    assertSame(player1, engine.getActivePlayer());
+  }
+
   @DisplayName(
       "It must not process a valid build connection request if the player has not enought resources")
   @Tag(value = "CatanBoardEngine")
@@ -55,9 +97,7 @@ public class CatanGameEngineTest {
       throws InvalidBoardDimensionsException, InvalidBoardElementException, NonNullInputException,
           NonVoidCollectionException {
 
-    Map<ResourceType, Integer> playerResources = new TreeMap<ResourceType, Integer>();
-
-    IPlayer player1 = new Player(0, new ResourceManager(playerResources));
+    IPlayer player1 = new Player(0, new ResourceManager());
     IPlayer[] players = {player1};
     ICatanEditableBoard board = buildStandardBoard(player1);
 
