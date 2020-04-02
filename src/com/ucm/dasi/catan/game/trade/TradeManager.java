85d92f5203333b36fce48fd8fd61d82b170e6332
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-04-02 20:42:40 +0200

    Added trade manager

---
+++ src/com/ucm/dasi/catan/game/trade/TradeManager.java
@@ -0,0 +1,84 @@
+package com.ucm.dasi.catan.game.trade;
+
+import java.util.Collection;
+import java.util.TreeMap;
+import java.util.UUID;
+
+import com.ucm.dasi.catan.game.exception.InvalidReferenceException;
+import com.ucm.dasi.catan.game.exception.NoCurrentTradeException;
+
+public class TradeManager implements ITradeManager {
+
+  private ITrade trade;
+  
+  private TreeMap<UUID, ITradeAgreement> tradeAgreements;
+  
+  public TradeManager() {
+    tradeAgreements = new TreeMap<UUID, ITradeAgreement>();
+  }
+  
+  @Override
+  public void addAgreement(ITradeAgreement agreement) {
+    
+    if (trade == null) {
+      throw new NoCurrentTradeException();
+    }
+    
+    if (!trade.getId().equals(agreement.getTrade().getId())) {
+      throw new InvalidReferenceException(trade, agreement.getTrade());
+    }
+    
+    tradeAgreements.put(agreement.getId(), agreement);
+  }
+  
+  @Override
+  public void confirm(ITradeConfirmation confirmation) {
+    
+    if (trade == null) {
+      throw new NoCurrentTradeException();
+    }
+    
+    if (!tradeAgreements.containsKey(confirmation.getAgreement().getId())) {
+      throw new InvalidReferenceException(confirmation.getAgreement());
+    }
+    
+    clear();
+  }
+  
+  @Override
+  public void discard() {
+    
+    if (trade == null) {
+      throw new NoCurrentTradeException();
+    }
+    
+    clear();
+  }
+  
+  @Override
+  public Collection<ITradeAgreement> getAgreements() {
+    
+    if (trade == null) {
+      throw new NoCurrentTradeException();
+    }
+    
+    return tradeAgreements.values();
+  }
+  
+  @Override
+  public ITrade getTrade() {
+    return trade;
+  }
+  
+  @Override
+  public void start(ITrade trade) {
+    this.trade = trade;
+    tradeAgreements.clear();
+  }
+  
+  private void clear() {
+    
+    trade = null;
+    tradeAgreements.clear();
+  }
+}
