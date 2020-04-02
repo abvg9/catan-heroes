5f87d9d8b551a97174f81b4df6bea59fc776ae59
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-04-02 20:42:23 +0200

    Trade requests refactor

--- src/com/ucm/dasi/catan/request/trade/ITradeAgreementRequest.java
+++ src/com/ucm/dasi/catan/request/trade/ITradeAgreementRequest.java
@@ -1,14 +1,9 @@
 package com.ucm.dasi.catan.request.trade;
 
+import com.ucm.dasi.catan.game.trade.ITradeAgreement;
 import com.ucm.dasi.catan.request.IRequest;
-import com.ucm.dasi.catan.resource.IResourceStorage;
-import java.util.UUID;
 
 public interface ITradeAgreementRequest extends IRequest {
 
-  IResourceStorage getExchange();
-
-  UUID getId();
-
-  UUID getRequestId();
+  ITradeAgreement getTradeAgreement();
 }
