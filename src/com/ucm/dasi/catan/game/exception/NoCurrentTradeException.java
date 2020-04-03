0233aacaa48c08312b6c0d5e7e0033676eca0fba
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-04-03 16:47:20 +0200

    Trade manager has been improved. Added missing constraints. Exceptions are no longer runtime exceptions.

--- src/com/ucm/dasi/catan/game/exception/NoCurrentTradeException.java
+++ src/com/ucm/dasi/catan/game/exception/NoCurrentTradeException.java
@@ -1,6 +1,6 @@
 package com.ucm.dasi.catan.game.exception;
 
-public class NoCurrentTradeException extends RuntimeException {
+public class NoCurrentTradeException extends Exception {
 
   private static final long serialVersionUID = -8491134590419623868L;
 
