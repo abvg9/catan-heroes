e75f0c6accee41769cca20836f82210e5242ebac
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-04-11 01:11:20 +0200

    Fixed javadoc

--- src/io/github/notaphplover/catan/core/game/point/IPointsCalculator.java
+++ src/io/github/notaphplover/catan/core/game/point/IPointsCalculator.java
@@ -8,7 +8,6 @@ public interface IPointsCalculator {
   /**
    * Gets the points achieved by every player
    *
-   * @param game Game instance to process.
    * @return Map of players to player points.
    */
   Map<IPlayer, Integer> getPoints();
