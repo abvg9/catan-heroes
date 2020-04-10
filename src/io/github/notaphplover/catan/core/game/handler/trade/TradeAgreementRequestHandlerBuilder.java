ec0378b4e84e256378ad5a74d8ef25eb2f9a9c39
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-04-11 00:27:33 +0200

    Added trade handlers

---
+++ src/io/github/notaphplover/catan/core/game/handler/trade/TradeAgreementRequestHandlerBuilder.java
@@ -0,0 +1,8 @@
+package io.github.notaphplover.catan.core.game.handler.trade;
+
+import io.github.notaphplover.catan.core.game.handler.StandardRequestHandlerBuilder;
+import io.github.notaphplover.catan.core.request.trade.ITradeAgreementRequest;
+
+public class TradeAgreementRequestHandlerBuilder
+    extends StandardRequestHandlerBuilder<
+        ITradeAgreementRequest, TradeAgreementRequestHandlerBuilder> {}
---
+++ src/io/github/notaphplover/catan/core/game/handler/trade/TradeRequestHandlerBuilder.java
@@ -0,0 +1,7 @@
+package io.github.notaphplover.catan.core.game.handler.trade;
+
+import io.github.notaphplover.catan.core.game.handler.StandardRequestHandlerBuilder;
+import io.github.notaphplover.catan.core.request.trade.ITradeRequest;
+
+public class TradeRequestHandlerBuilder
+    extends StandardRequestHandlerBuilder<ITradeRequest, TradeRequestHandlerBuilder> {}
